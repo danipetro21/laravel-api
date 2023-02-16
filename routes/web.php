91bea9e7f555265193b46cda22aaa82d3a05eeb5
danipetro21 <idany346@gmail.com>
2023-02-16 15:36:23 +0100

    add apiController and route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\ApiController;
+
+Route :: get('/api/v1/movie/all', [ApiController :: class, 'movieAll']);
